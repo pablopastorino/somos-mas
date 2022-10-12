8cb6b67702d79abe83087dee7446efbab3f71f26
Pablo Pastorino <phpastorino@outlook.com>
2022-10-12 18:00:18 -0300

    Removes credentials from code

--- utils/S3.js
+++ utils/S3.js
@@ -2,19 +2,19 @@ import { v4 as uuidv4 } from 'uuid'
 import AWS from 'aws-sdk'
 
 AWS.config.update({
-  accessKeyId: 'AKIAQCNAUO4PD2O7HV5K',
-  secretAccessKey: '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'
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 const myBucket = new AWS.S3({
-  params: { Bucket: 'somos-mas' },
-  region: 'us-east-1'
+  params: { Bucket: process.env.AWS_BUCKET_NAME },
+  region: process.env.AWS_REGION
 })
 
 export const uploadFile = async file => {
   const params = {
     Body: file,
-    Bucket: 'somos-mas',
+    Bucket: process.env.AWS_BUCKET_NAME,
     Key: `uploads/${uuidv4()}-${file.name}`
   }
 
